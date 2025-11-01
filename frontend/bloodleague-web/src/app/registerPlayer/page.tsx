3e85fe0a8ffe02c88461ff8199a38fb2f173d9a5
belezoro <aotero014@gmail.com>
2025-11-01 16:45:43 +0100

    deploy postgres x14

--- frontend/bloodleague-web/src/app/registerPlayer/page.tsx
+++ frontend/bloodleague-web/src/app/registerPlayer/page.tsx
@@ -23,6 +23,7 @@ export default function RegisterPlayer() {
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
         // Aquí puedes manejar el registro del jugador usando los valores de nick, email y password
+        console.log(process.env.NEXT_PUBLIC_API_URL);
         axios.post(`${process.env.NEXT_PUBLIC_API_URL}/players/createPlayer`, player)
             .then(response => {
                 setMessage("Jugador registrado exitosamente");
